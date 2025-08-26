a8dd6146ff7a5a918c93b9c2d75e18a752667b59
Suryanandan Babbar <suryanandanbabbar@gmail.com>
2025-08-26 17:23:49 +0530

    Initial Commit

---
+++ Scout/ScoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoutApp.swift
+//  Scout
+//
+//  Created by Suryanandan Babbar on 26/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
